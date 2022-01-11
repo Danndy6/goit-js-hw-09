fe32afda1614e973d2a97b45222ab6b6693ed8d0
Danndy6 <danndy754@gmail.com>
2022-01-11 20:03:27 +0200

    hw_09_1/3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+"use strict"
 
+const refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+    bodyColor: document.querySelector("body"),
+};
+let timedId = null;
+refs.stopBtn.setAttribute("disabled", true);
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+    timedId = setInterval(() => {
+        refs.bodyColor.style.backgroundColor = getRandomHexColor() 
+    }, 1000);
+    refs.startBtn.setAttribute("disabled", true);
+    refs.stopBtn.removeAttribute("disabled");
+}
+
+function onStopBtnClick() {
+    clearInterval(timedId);
+    refs.startBtn.removeAttribute("disabled");
+    refs.stopBtn.setAttribute("disabled", true);
+}
